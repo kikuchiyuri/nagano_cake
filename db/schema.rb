566259d227887f7f72e36f2b4563f6d42713f9ff
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-13 13:27:03 +0000

    [Add]genresテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_132058) do
+ActiveRecord::Schema.define(version: 2022_07_13_132540) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2022_07_13_132058) do
     t.boolean "is_active"
     t.index ["email"], name: "index_costomers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_costomers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "items", force: :cascade do |t|
