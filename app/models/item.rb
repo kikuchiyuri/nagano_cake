d97f861e65207e506edb7ddd0ffbc2c686442274
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-17 21:14:00 +0000

    [Fix]画像のサイズをモデルで指定する（Gemfileの修正）

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
   # 商品画像投稿
   has_one_attached :image
   # 画像のサイズ変更
-  #def image(width, height)
-    #image.variant(resize_to_limit: [width, height]).processed
-  #end
+  def get_image(width, height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 
   # 10%の消費税を加えた税込価格のメソッドを定義
   def add_tax_price
