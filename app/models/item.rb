d3f777aad94dfe263b926676987b8b8f376077f8
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-17 20:28:12 +0000

    [Update]商品詳細ページ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,17 @@ class Item < ApplicationRecord
 
   belongs_to :genre
 
-#商品画像投稿
+  # 商品画像投稿
   has_one_attached :image
-#画像のサイズ変更
-  #def image(width,height)
-    #image.variant(resize_to_limit: [width, height]).processed
-  #end
+  # 画像のサイズ変更
+   #def image(width,height)
+     #image.variant(resize_to_limit: [width, height]).processed
+   #end
+
+  # 10%の消費税を加えた税込価格のメソッドを定義
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
 
 end
