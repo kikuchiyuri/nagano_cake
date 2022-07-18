2c960c8cdcc8de2e6aded3d242ec22af6c9f0bde
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-18 09:59:34 +0000

    [Add]会員情報新規登録画面

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -69,7 +69,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_psermitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
   end
 
 end
