289589089fe1fe96c1163e41d4266f86ab7e7ff1
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-21 12:46:00 +0000

    [Add]admin/ordersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
   ##管理者用
   #URL /admin/sign_in...
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
