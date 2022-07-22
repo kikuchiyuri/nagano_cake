642233593198b86b6755906da07218de66ec91cd
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-22 13:02:13 +0000

    [Fix]admin/ordersのルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-  end
   ##管理者用
   #URL /admin/sign_in...
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
@@ -16,6 +11,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show]
   end
 
 
