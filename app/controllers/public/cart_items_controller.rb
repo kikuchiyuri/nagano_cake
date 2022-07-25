4ecff1c47992f750847c615645d6269ff4cbcf52
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-25 12:03:05 +0000

    [Add]find_byメソッドの実装（途中）

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -24,8 +24,14 @@ class Public::CartItemsController < ApplicationController
   def create
     cart_item = CartItem.new(cart_item_params)
     cart_item.customer_id = current_customer.id
-    cart_item.save
-    redirect_to cart_items_path
+    if Item.find_by(params[:item_id]).present?
+      Item.find_by(params[:item_id]).cart_item.amount += params[:item_id].to_i
+      cart_item.find_by(params).save
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   private
