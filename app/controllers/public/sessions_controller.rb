5f9fe391222290384138b1294e329712f3932d6a
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-28 14:12:20 +0000

    [Update]退会後のログイン阻止機能　途中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     root_path
@@ -26,10 +27,26 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # 退会処理
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 入力されたemailからアカウントを一件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを修了する（→会員は新規登録する必要がある）
+    return if !@customer
+    ## 取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    if @customer.valid_passward?(params[:customer][:passward])
+      redirect_to new_customer_registration_path
+    else
+      redirect_to action: :create
+    end
+
+  end
 end
