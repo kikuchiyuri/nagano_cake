3a583ef779e9c47425141cbfd5b2ed1fcef9dea4
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-09-11 13:23:43 +0000

    [Uupdate]注文ステータスのenum設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,5 @@ class Order < ApplicationRecord
   has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+  enum status: { before_payment: 0, confirm_pament: 1, making_cakes: 2, preparation: 3, done: 4 }
 end
