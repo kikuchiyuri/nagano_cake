e9eee1069167d1ec4db1f2a1832831bf112e1763
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-09-19 14:14:00 +0000

    [Fix]配送先

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -30,7 +30,7 @@ class Public::AddressesController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:name, :postal_cade, :address)
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 
 end
