441ad9d9072b2255c6f06e213c1a6c3ae3037754
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-09-19 13:02:32 +0000

    [Update]配送先登録画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,23 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    #@addresses = Address.find(params[:id])
+  end
+
+  def create
+    address = Address.new(address_params)
+    address.save
+    redirect_to addresses_path
   end
 
   def edit
   end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_cade, :address)
+  end
+
 end
